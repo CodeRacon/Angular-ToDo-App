4e262da3eb81602484d945ce0cd48045ad8e5a82
CodeRacon <michael.buschmann.86@gmail.com>
2024-06-11 01:37:37 +0200

    used property binding , eventListeners and getters do simplify template code

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
 export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex()];
 
+  get imgPath() {
+    return 'assets/img/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    this.selectedUser = DUMMY_USERS[randomIndex()];
+  }
 }
